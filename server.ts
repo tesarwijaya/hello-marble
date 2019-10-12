d143328a66b82e6ec474cb5ea41dc289193fddba
Tesar Wijaya <tesarwijaya@Tesars-MacBook-Pro.local>
2019-10-12 20:33:29 +0700

    [MODIFY] code convention by eslint

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-import { createContext } from '@marblejs/core';
-import * as http from 'http';
-import httpListener from './http.listener';
+import { createContext } from '@marblejs/core'
+import * as http from 'http'
+import httpListener from './http.listener'
 
 const httpServer = httpListener
-  .run(createContext());
+  .run(createContext())
 
+// eslint-disable-next-line import/prefer-default-export
 export const server = http
   .createServer(httpServer)
-  .listen(1337, '0.0.0.0');
+  .listen(1337, '0.0.0.0')
